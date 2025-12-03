c048e50870734d315f7d5a9b9668296c0d3aba39
River Route Creative Group <riverroutecg@gmail.com>
2025-12-03 17:13:37 +0530

    03/12

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const path = require('path')
 const mysql = require("mysql2")
 
 app.use(cors())
@@ -17,5 +18,21 @@ dbconn.connect((err)=> {
     if(err)
         console.log(err);
     console.log("mysql connected successfully");
-    
+})
+
+app.get("/admin_login", (req, res) => {
+    console.log("hello");
+
+    res.send("Admin Login Page Placeholder");
+})
+
+
+app.use((req, res) => {
+    // If the request makes it here, the route wasn't matched.
+    res.status(404).send("Error: Route not found on Express server.");
+});
+
+
+app.listen(3000, () =>{
+    console.log("Server running on port 3000");
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import Admin_login from './admin_components/Admin_login'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-        <Admin_login />
+        {/* <Admin_login /> */}
+        {/* <BrowserRouter>
+          <Routes>
+            <Route path="/admin_login" element={<Admin_login />} />
+          </Routes>
+        </BrowserRouter> */}
     </>
   )
 }
